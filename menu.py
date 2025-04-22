f19fc9fdc84e6a96288927450052e8dd5e8c513b
Hurrtt <Ethangarcia3743@gmail.com>
2025-04-21 21:20:03 -0600

    Agregamos el menu

---
+++ menu.py
@@ -0,0 +1,18 @@
+def menu():
+    print("Ingrese una opcion: \n1-Agregar tarea \n2-Ver tareas pendientes \n3-Marcar tarea completada \n4-Eliminar tarea \n5-Salir")
+    opc = input()
+    while opc != '5':
+        if opc == '1':
+            agregar_tarea()
+        if opc == '2':
+            tareas_pendientes()
+        if opc == '3':
+            complete()
+        if opc == '4':
+            eliminar_tareas()
+        if opc == '5':
+            print("Hasta luego")
+        else:
+            print("Elija una opcion valida...")
+            return menu()
+menu()
